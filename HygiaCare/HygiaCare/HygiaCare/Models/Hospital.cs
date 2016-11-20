ab1cffbb7d029112990da92fd7107558ff1089e5
Wsurpador <Wsurpador@DESKTOP-83L2KG3>
2016-11-20 21:27:16 +0100

    Agregado cambios graficos mas imagen

--- HygiaCare/HygiaCare/HygiaCare/Models/Hospital.cs
+++ HygiaCare/HygiaCare/HygiaCare/Models/Hospital.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace HygiaCare.Models
 {
@@ -11,5 +12,6 @@ namespace HygiaCare.Models
         public String Name { get; set; }
         public String ComunidadAutonoma { get; set; }
 
+     
     }
 }
--- HygiaCare/HygiaCare/HygiaCare/Services/HospitalesServices.cs
+++ HygiaCare/HygiaCare/HygiaCare/Services/HospitalesServices.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace HygiaCare.Services
 {
@@ -17,7 +18,7 @@ namespace HygiaCare.Services
                 new Hospital
                 {
                        Name = "Vinalopo",
-                       ComunidadAutonoma = "Comunidad Valenciana"
+                       ComunidadAutonoma = "Comunidad Valenciana",
                 },
                 new Hospital
                 {
