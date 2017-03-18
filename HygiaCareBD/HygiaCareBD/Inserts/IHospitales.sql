72eb0fd390b3bae783e759841ce6d4127f06a5fb
jamolpe <jamolpe@gmail.com>
2017-03-18 23:38:26 +0100

    Borrado proyecto incompleto

--- HygiaCareBD/HygiaCareBD/Inserts/IHospitales.sql
+++
@@ -1,6 +0,0 @@
-/*insert into Hospitales values('Torrevieja','Comunidad Valenciana');
-
-insert into Hospitales values('Vinalopo','Comunidad Valenciana');
-
-insert into Hospitales values('Alcira','Comunidad Valenciana');
-*/
--- HygiaCareBD/HygiaCareBD/Tablas/Create_table_Urgencias.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Urgencias
-(
-	id int IDENTITY (1,1) not null, 
-	CantidadUsuarios int NULL, 
-	Hospital int not null FOREIGN KEY REFERENCES Hospitales(id),
-    CONSTRAINT PK_Urgencias PRIMARY KEY (id)
-)
-GO
