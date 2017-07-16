e00b036a1631cce208d525ea889a93d862c69040
jamolpe <jamolpeeps@gmail.com>
2017-07-16 17:36:03 +0200

    Grafica modificada

--- Hygia/Hygia/Hygia/View/HospitalInfo.xaml.cs
+++ Hygia/Hygia/Hygia/View/HospitalInfo.xaml.cs
@@ -8,6 +8,9 @@ using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 using System.Threading.Tasks;
 using static Hygia.ViewModel.GoogleDirectionsJSONTranslation;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Axes;
 
 namespace Hygia.View
 {
@@ -31,7 +34,7 @@ namespace Hygia.View
             this.hospital = hospital;
             //this.Titulo.Text = hospital.Nombre;
             this.hospital = hospital;
-            
+
             AddPin();
             MoveToPing();
             obtenerDatosPantalla();
@@ -40,9 +43,9 @@ namespace Hygia.View
 
         public void AddPin()
         {
-		Double x = Double.Parse(hospital.coordenadaX.Replace(".",","));
-		Double y = Double.Parse(hospital.coordenadaY.Replace(".",","));
-		position = new Position(x,y);
+            Double x = Double.Parse(hospital.coordenadaX.Replace(".", ","));
+            Double y = Double.Parse(hospital.coordenadaY.Replace(".", ","));
+            position = new Position(x, y);
             var pin = new Pin
             {
                 Type = PinType.Place,
@@ -57,29 +60,34 @@ namespace Hygia.View
             MyMap.MoveToRegion(new MapSpan(position, 0.02, 0.02));
         }
 
-        public async void obtenerDatosPantalla(){
+        public async void obtenerDatosPantalla()
+        {
 
             if (await Information())
             {
-                InformacionOcupacion();
+                if (await InformacionOcupacion())
+                {
+                    trabajarGrafo();
+                };
             }
         }
         public async Task<bool> Information()
         {
             MapsGest = new WorkingMaps();
 
-            if(await MapsGest.GetActualPosition())
+            if (await MapsGest.GetActualPosition())
             {
                 await MapsGest.GetJSONDirecciones(hospital.coordenadaX, hospital.coordenadaY, hospital.coordenadaZ);
                 distancia = MapsGest.GetDistancia();
                 Tiempo = MapsGest.GetTiempo();
 
-            }else
+            }
+            else
             {
                 distancia = "N|N";
                 Tiempo = "N|N";
             }
-            
+
             LblDistancia.Text = distancia;
             LblTiempo.Text = Tiempo;
             ACCargandoTiempo.IsRunning = false;
@@ -87,16 +95,57 @@ namespace Hygia.View
             return true;
         }
 
-        public async void InformacionOcupacion(){
+        public async Task<bool> InformacionOcupacion()
+        {
             info = new HospitalInfoViewModel();
 
-            if(await info.ObtenerOcupacionAPI(hospital.id)){
+            if (await info.ObtenerOcupacionAPI(hospital.id))
+            {
                 OcupacionH = info.ObtenerOcupacion();
+                return true;
             }
+            return false;
+
         }
 
+        public void trabajarGrafo()
+        {
+            var modelEx = new PlotModel { Title = "Example 1" };
+            this.graph.Model = GraficoHoras().Result;
+        }
+        public async Task<PlotModel> GraficoHoras()
+        {
+            var plotModel = new PlotModel { Title = "Ocupacion por horas" };
 
+            plotModel.Axes.Add(new OxyPlot.Axes.LinearAxis { Position = AxisPosition.Bottom });
+            plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Maximum = 10, Minimum = 0 });
 
+            var series1 = new LineSeries
+            {
+                MarkerType = MarkerType.Star,
+                MarkerSize = 4,
+                MarkerStroke = OxyColors.White
+            };
+            foreach(OcupacionHoras ocupacion in OcupacionH){
+                series1.Points.Add(new DataPoint(0.0, 6.0));
+            }
+
+            series1.Points.Add(new DataPoint(1.4, 2.1));
+            series1.Points.Add(new DataPoint(2.0, 4.2));
+            series1.Points.Add(new DataPoint(3.3, 2.3));
+            series1.Points.Add(new DataPoint(4.7, 7.4));
+            series1.Points.Add(new DataPoint(6.0, 6.2));
+            series1.Points.Add(new DataPoint(8.9, 8.9));
+
+            plotModel.Series.Add(series1);
+
+            return plotModel;
+        }
+
+        /*public int formatearHoras(String hora){
+            
+        }
+*/
     }
 }
    
